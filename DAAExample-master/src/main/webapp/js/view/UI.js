c0d409a41dd0e5f7ac4ba7ba1c4aebd9480e17c3
jjimenezg93 <jjimenezg93@gmail.com>
2015-03-26 17:52:14 +0100

    Creada estructura base para Eventos Recomendados
    Falta añadir objeto evento y las llamadas correspondientes.

--- DAAExample-master/src/main/webapp/js/view/UI.js
+++ DAAExample-master/src/main/webapp/js/view/UI.js
@@ -1,88 +1,50 @@
-function insertMenuTop(parent) {
-	parent.append(
-		'<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">\
-	        <div class="container">\
-	            <!-- Brand and toggle get grouped for better mobile display -->\
-	            <div class="navbar-header">\
-	                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">\
-	                    <span class="sr-only">Toggle navigation</span>\
-	                    <span class="icon-bar"></span>\
-	                    <span class="icon-bar"></span>\
-	                    <span class="icon-bar"></span>\
-	                </button>\
-	                <a class="navbar-brand" href="index.html">Start Bootstrap</a>\
-	            </div>\
-	            <!-- Collect the nav links, forms, and other content for toggling -->\
-	            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
-	                <ul class="nav navbar-nav navbar-right">\
-	                    <li>\
-	                        <a href="about.html">About</a>\
-	                    </li>\
-	                    <li>\
-	                        <a href="services.html">Services</a>\
-	                    </li>\
-	                    <li>\
-	                        <a href="contact.html">Contact</a>\
-	                    </li>\
-	                    <li class="dropdown">\
-	                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <b class="caret"></b></a>\
-	                        <ul class="dropdown-menu">\
-	                            <li>\
-	                                <a href="portfolio-1-col.html">1 Column Portfolio</a>\
-	                            </li>\
-	                            <li>\
-	                                <a href="portfolio-2-col.html">2 Column Portfolio</a>\
-	                            </li>\
-	                            <li>\
-	                                <a href="portfolio-3-col.html">3 Column Portfolio</a>\
-	                            </li>\
-	                            <li>\
-	                                <a href="portfolio-4-col.html">4 Column Portfolio</a>\
-	                            </li>\
-	                            <li>\
-	                                <a href="portfolio-item.html">Single Portfolio Item</a>\
-	                            </li>\
-	                        </ul>\
-	                    </li>\
-	                    <li class="dropdown">\
-	                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <b class="caret"></b></a>\
-	                        <ul class="dropdown-menu">\
-	                            <li>\
-	                                <a href="blog-home-1.html">Blog Home 1</a>\
-	                            </li>\
-	                            <li>\
-	                                <a href="blog-home-2.html">Blog Home 2</a>\
-	                            </li>\
-	                            <li>\
-	                                <a href="blog-post.html">Blog Post</a>\
-	                            </li>\
-	                        </ul>\
-	                    </li>\
-	                    <li class="dropdown">\
-	                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Other Pages <b class="caret"></b></a>\
-	                        <ul class="dropdown-menu">\
-	                            <li>\
-	                                <a href="full-width.html">Full Width Page</a>\
-	                            </li>\
-	                            <li>\
-	                                <a href="sidebar.html">Sidebar Page</a>\
-	                            </li>\
-	                            <li>\
-	                                <a href="faq.html">FAQ</a>\
-	                            </li>\
-	                            <li>\
-	                                <a href="404.html">404</a>\
-	                            </li>\
-	                            <li>\
-	                                <a href="pricing.html">Pricing Table</a>\
-	                            </li>\
-	                        </ul>\
-	                    </li>\
-	                </ul>\
-	            </div>\
-	            <!-- /.navbar-collapse -->\
-	        </div>\
-	        <!-- /.container -->\
-	    </nav>'
-	);
+function insertDetailedEvent(parent) {
+	parent.append('<!-- Features Section -->\
+        <div class="row">\
+            <div class="col-lg-12">\
+                <h2 class="page-header">AQUI IRIA EL NOMBRE DEL EVENTO</h2>\
+            </div>\
+            <div class="col-md-6">\
+                <ul>\
+					<li><strong>Creador: </strong><a href="#">pepito</a></li>\
+                    <li><strong>Fecha: </strong>AQUI IRIA LA FECHA</li>\
+                    <li><strong>Tiempo restante: </strong>AQUI VA EL TIEMPO RESTANTE</li>\
+                    <li><strong>Participantes: </strong>AQUI VA EL NUMERO DE PARTICIPANTES</li>\
+                </ul>\
+                <p>The Modern Business template by Start Bootstrap includes:\
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>\
+            </div>\
+            <div class="col-md-4 .col-md-offset-4">\
+                <img class="img-responsive" src="http://placehold.it/700x450" alt="">\
+            </div>\
+        </div>\
+        <!-- /.row -->')
+}
+
+function insertRecommended(parent) {
+	parent.append('<div class="row">\
+            <div class="col-lg-12">\
+            <h2 class="page-header">Eventos recomendados</h2>\
+        </div>\
+        <div class="col-md-3 col-sm-6">\
+            <a href="portfolio-item.html">\
+                <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">\
+            </a>\
+        </div>\
+        <div class="col-md-3 col-sm-6">\
+            <a href="portfolio-item.html">\
+                <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">\
+            </a>\
+        </div>\
+        <div class="col-md-3 col-sm-6">\
+            <a href="portfolio-item.html">\
+                <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">\
+            </a>\
+        </div>\
+        <div class="col-md-3 col-sm-6">\
+            <a href="portfolio-item.html">\
+                <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">\
+            </a>\
+        </div>\
+    </div>')
 }
