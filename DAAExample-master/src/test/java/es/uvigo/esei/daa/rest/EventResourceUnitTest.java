16003c0b30099317acc3c7c27a96fb236789088f
mcpaz <carpazmarcos@gmail.com>
2015-05-06 10:12:24 +0200

    Refactorización 2.0
    Revisión y corrección

--- DAAExample-master/src/test/java/es/uvigo/esei/daa/rest/EventResourceUnitTest.java
+++ DAAExample-master/src/test/java/es/uvigo/esei/daa/rest/EventResourceUnitTest.java
@@ -1,11 +1,9 @@
 
 package es.uvigo.esei.daa.rest;
 
-import static org.easymock.EasyMock.anyInt;
-import static org.easymock.EasyMock.anyString;
+
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
@@ -17,12 +15,10 @@ import java.util.List;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.glassfish.grizzly.http.util.TimeStamp;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import es.uvigo.esei.daa.dao.DAOException;
 import es.uvigo.esei.daa.dao.EventDAO;
 import es.uvigo.esei.daa.entities.Event;
 
@@ -76,127 +72,5 @@ public class EventResourceUnitTest {
 		assertEquals(Status.OK, response.getStatusInfo());
 	}
 	
-	/*@Test
-	public void testListDAOException() throws Exception {
-		expect(daoMock.list()).andThrow(new DAOException());
-		replay(daoMock);
-		
-		final Response response = resource.list();
-		assertEquals(Status.INTERNAL_SERVER_ERROR, response.getStatusInfo());
-	}
-
-	@Test
-	public void testGet() throws Exception {
-		final Person person = new Person(1, "Pepe", "Pérez");
-		
-		expect(daoMock.get(person.getId())).andReturn(person);
-		replay(daoMock);
-		
-		final Response response = resource.get(person.getId());
-		assertEquals(person, response.getEntity());
-		assertEquals(Status.OK, response.getStatusInfo());
-	}
-
-	@Test
-	public void testGetDAOException() throws Exception {
-		expect(daoMock.get(anyInt())).andThrow(new DAOException());
-		replay(daoMock);
-		
-		final Response response = resource.get(1);
-		assertEquals(Status.INTERNAL_SERVER_ERROR, response.getStatusInfo());
-	}
-
-	@Test
-	public void testGetIllegalArgumentException() throws Exception {
-		expect(daoMock.get(anyInt())).andThrow(new IllegalArgumentException());
-		replay(daoMock);
-		
-		final Response response = resource.get(1);
-		assertEquals(Status.BAD_REQUEST, response.getStatusInfo());
-	}
 	
-	@Test
-	public void testDelete() throws Exception {
-		daoMock.delete(anyInt());
-		replay(daoMock);
-		
-		final Response response = resource.delete(1);
-		assertEquals(Status.OK, response.getStatusInfo());
-	}
-
-	@Test
-	public void testDeleteDAOException() throws Exception {
-		daoMock.delete(anyInt());
-		expectLastCall().andThrow(new DAOException());
-		replay(daoMock);
-		
-		final Response response = resource.delete(1);
-		assertEquals(Status.INTERNAL_SERVER_ERROR, response.getStatusInfo());
-	}
-
-	@Test
-	public void testDeleteIllegalArgumentException() throws Exception {
-		daoMock.delete(anyInt());
-		expectLastCall().andThrow(new IllegalArgumentException());
-		replay(daoMock);
-		
-		final Response response = resource.delete(1);
-		assertEquals(Status.BAD_REQUEST, response.getStatusInfo());
-	}
-
-	@Test
-	public void testModify() throws Exception {
-		final Person person = new Person(1, "Pepe", "Pérez");
-		
-		expect(daoMock.modify(person.getId(), person.getName(), person.getSurname()))
-			.andReturn(person);
-		replay(daoMock);
-		
-
-		final Response response = resource.modify(
-			person.getId(), person.getName(), person.getSurname());
-		assertEquals(person, response.getEntity());
-		assertEquals(Status.OK, response.getStatusInfo());
-	}
-
-	@Test
-	public void testModifyDAOException() throws Exception {
-		expect(daoMock.modify(anyInt(), anyString(), anyString()))
-			.andThrow(new DAOException());
-		replay(daoMock);
-
-		final Response response = resource.modify(1, "Paco", "Pérez");
-		assertEquals(Status.INTERNAL_SERVER_ERROR, response.getStatusInfo());
-	}
-
-	@Test
-	public void testModifyIllegalArgumentException() throws Exception {
-		expect(daoMock.modify(anyInt(), anyString(), anyString()))
-			.andThrow(new IllegalArgumentException());
-		replay(daoMock);
-		
-		final Response response = resource.modify(1, "Paco", "Pérez");
-		assertEquals(Status.BAD_REQUEST, response.getStatusInfo());
-	}
-
-
-	@Test
-	public void testAddDAOException() throws Exception {
-		expect(daoMock.add(anyString(), anyString()))
-			.andThrow(new DAOException());
-		replay(daoMock);
-
-		final Response response = resource.add("Paco", "Pérez");
-		assertEquals(Status.INTERNAL_SERVER_ERROR, response.getStatusInfo());
-	}
-
-	@Test
-	public void testAddIllegalArgumentException() throws Exception {
-		expect(daoMock.add(anyString(), anyString()))
-			.andThrow(new IllegalArgumentException());
-		replay(daoMock);
-		
-		final Response response = resource.add("Paco", "Pérez");
-		assertEquals(Status.BAD_REQUEST, response.getStatusInfo());
-	}*/
 }
